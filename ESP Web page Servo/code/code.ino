365efe9dd800de1cbbefebdf5c5e1c1546794289
RumixonElectro <106797730+RumixonElectro@users.noreply.github.com>
2026-05-12 02:37:49 +0530

    Update code.ino

--- ESP Web page Servo/code/code.ino
+++ ESP Web page Servo/code/code.ino
@@ -1,130 +1,97 @@
 #include <WiFi.h>
 #include <ESP32Servo.h>
 
-Servo myservo;  // create servo object to control a servo
-// twelve servo objects can be created on most boards
+const int numServos = 6;
+Servo joints[numServos];
+int pins[numServos] = {13, 12, 14, 27, 26, 25}; 
 
-// GPIO the servo is attached to
-static const int servoPin = 13;
+const char* ssid     = "Airtel_balu_2470_ex";
+const char* password = "air38583";
 
-// Replace with your network credentials
-const char* ssid     = "REPLACE_WITH_YOUR_SSID";
-const char* password = "REPLACE_WITH_YOUR_PASSWORD";
-
-// Set web server port number to 80
 WiFiServer server(80);
-
-// Variable to store the HTTP request
 String header;
-
-// Decode HTTP GET value
-String valueString = String(5);
-int pos1 = 0;
-int pos2 = 0;
-
-// Current time
-unsigned long currentTime = millis();
-// Previous time
-unsigned long previousTime = 0; 
-// Define timeout time in milliseconds (example: 2000ms = 2s)
-const long timeoutTime = 2000;
 
 void setup() {
   Serial.begin(115200);
 
-  myservo.attach(servoPin);  // attaches the servo on the servoPin to the servo object
+  // ESP32 Timer allocation
+  ESP32PWM::allocateTimer(0);
+  ESP32PWM::allocateTimer(1);
+  ESP32PWM::allocateTimer(2);
+  ESP32PWM::allocateTimer(3);
 
-  // Connect to Wi-Fi network with SSID and password
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+  for (int i = 0; i < numServos; i++) {
+    joints[i].setPeriodHertz(50);
+    joints[i].attach(pins[i], 500, 2400);
+    joints[i].write(90);
   }
-  // Print local IP address and start web server
-  Serial.println("");
-  Serial.println("WiFi connected.");
-  Serial.println("IP address: ");
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) { delay(500); Serial.print("."); }
+  
+  Serial.println("\nWiFi connected.");
   Serial.println(WiFi.localIP());
   server.begin();
 }
 
-void loop(){
-  WiFiClient client = server.available();   // Listen for incoming clients
-
-  if (client) {                             // If a new client connects,
-    currentTime = millis();
-    previousTime = currentTime;
-    Serial.println("New Client.");          // print a message out in the serial port
-    String currentLine = "";                // make a String to hold incoming data from the client
-    while (client.connected() && currentTime - previousTime <= timeoutTime) { // loop while the client's connected
+void loop() {
+  WiFiClient client = server.available();
+  if (client) {
+    String currentLine = "";
+    unsigned long currentTime = millis();
+    unsigned long previousTime = currentTime;
+    
+    while (client.connected() && currentTime - previousTime <= 2000) {
       currentTime = millis();
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        Serial.write(c);                    // print it out the serial monitor
+      if (client.available()) {
+        char c = client.read();
         header += c;
-        if (c == '\n') {                    // if the byte is a newline character
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
+        if (c == '\n') {
           if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
+            // Send Headers
             client.println("HTTP/1.1 200 OK");
             client.println("Content-type:text/html");
             client.println("Connection: close");
             client.println();
 
-            // Display the HTML web page
-            client.println("<!DOCTYPE html><html>");
-            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-            client.println("<link rel=\"icon\" href=\"data:,\">");
-            // CSS to style the on/off buttons 
-            // Feel free to change the background-color and font-size attributes to fit your preferences
-            client.println("<style>body { text-align: center; font-family: \"Trebuchet MS\", Arial; margin-left:auto; margin-right:auto;}");
-            client.println(".slider { width: 300px; }</style>");
-            client.println("<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js\"></script>");
-                     
-            // Web Page
-            client.println("</head><body><h1>ESP32 with Servo</h1>");
-            client.println("<p>Position: <span id=\"servoPos\"></span></p>");          
-            client.println("<input type=\"range\" min=\"0\" max=\"180\" class=\"slider\" id=\"servoSlider\" onchange=\"servo(this.value)\" value=\""+valueString+"\"/>");
-            
-            client.println("<script>var slider = document.getElementById(\"servoSlider\");");
-            client.println("var servoP = document.getElementById(\"servoPos\"); servoP.innerHTML = slider.value;");
-            client.println("slider.oninput = function() { slider.value = this.value; servoP.innerHTML = this.value; }");
-            client.println("$.ajaxSetup({timeout:1000}); function servo(pos) { ");
-            client.println("$.get(\"/?value=\" + pos + \"&\"); {Connection: close};}</script>");
-           
-            client.println("</body></html>");     
-            
-            //GET /?value=180& HTTP/1.1
-            if(header.indexOf("GET /?value=")>=0) {
-              pos1 = header.indexOf('=');
-              pos2 = header.indexOf('&');
-              valueString = header.substring(pos1+1, pos2);
+            // Web Page Content
+            client.println("<!DOCTYPE html><html><head>");
+            client.println("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<style>body{text-align:center;font-family:sans-serif;background:#222;color:white;} .s{width:80%;height:25px;}</style>");
+            client.println("</head><body><h1>Arm Control</h1>");
+
+            // Generate 6 Sliders
+            for(int i=0; i<6; i++) {
+              client.print("<div>Joint "); client.print(i);
+              client.print(": <span id=\"v"); client.print(i); client.print("\">90</span><br>");
+              client.print("<input type=\"range\" min=\"0\" max=\"180\" value=\"90\" class=\"s\" ");
+              client.print("oninput=\"move("); client.print(i); client.print(",this.value)\"></div><br>");
+            }
+
+            // JavaScript (Pure JS - No jQuery needed)
+            client.println("<script>function move(j,a){");
+            client.println("document.getElementById('v'+j).innerHTML=a;");
+            client.println("fetch('/?j='+j+'&a='+a);}</script>");
+            client.println("</body></html>");
+
+            // Parse Command: GET /?j=0&a=90
+            if(header.indexOf("GET /?j=") >= 0) {
+              int jIdx = header.indexOf("j=") + 2;
+              int jVal = header.substring(jIdx, header.indexOf("&")).toInt();
+              int aIdx = header.indexOf("a=") + 2;
+              int aVal = header.substring(aIdx, header.indexOf(" ", aIdx)).toInt();
               
-              //Rotate the servo
-              myservo.write(valueString.toInt());
-              Serial.println(valueString); 
-            }         
-            // The HTTP response ends with another blank line
-            client.println();
-            // Break out of the while loop
+              if(jVal >= 0 && jVal < 6) {
+                joints[jVal].write(aVal);
+                Serial.printf("Moving Joint %d to %d\n", jVal, aVal);
+              }
+            }
             break;
-          } else { // if you got a newline, then clear currentLine
-            currentLine = "";
-          }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
-        }
+          } else { currentLine = ""; }
+        } else if (c != '\r') { currentLine += c; }
       }
     }
-    // Clear the header variable
     header = "";
-    // Close the connection
     client.stop();
-    Serial.println("Client disconnected.");
-    Serial.println("");
   }
 }
